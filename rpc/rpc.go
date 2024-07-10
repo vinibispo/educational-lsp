8387e906c3a5d84fe82ddf936681a7b61c44254b
vinibispo <vini.bispo015@gmail.com>
2024-07-09 22:17:58 -0300

    Create silly DecodeMessage (just to mark progress)

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,8 +1,11 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 func EncodeMessage(msg any) string {
@@ -12,4 +15,23 @@ func EncodeMessage(msg any) string {
   }
 
   return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMessage(msg []byte) (int, error) {
+  header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+
+  if !found {
+    return 0, errors.New("Did not find separator")
+  }
+
+  contentLengthBytes := header[len("Content-Length: "):]
+  contentLength, err := strconv.Atoi(string(contentLengthBytes))
+
+  _ = content
+
+  if err != nil {
+    return 0, err
+  }
+
+  return contentLength, nil
 }
--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -17,3 +17,16 @@ func TestEncode(t *testing.T) {
     t.Fatalf("Expected %s, got %s", expected, actual)
   }
 }
+
+func TestDecode(t *testing.T) {
+  expected := 16
+  actual, err := rpc.DecodeMessage([]byte("Content-Length: 16\r\n\r\n{\"testing\":true}"))
+
+  if err != nil {
+    t.Fatalf("Unexpected error: %s", err)
+  }
+
+  if actual != expected {
+    t.Fatalf("Expected %d, got %d", expected, actual)
+  }
+}
